20c9cbef633c3cb701f92d158b44b7faaf4d0d8c
Adrian Mille <adrmille@users.noreply.github.com>
2018-12-23 20:38:34 +0100

    #1 Init Springboot project

---
+++ smoketest/src/main/java/com/amille/smoketest/SmoketestApplication.java
@@ -0,0 +1,14 @@
+package com.amille.smoketest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoketestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoketestApplication.class, args);
+	}
+
+}
+
