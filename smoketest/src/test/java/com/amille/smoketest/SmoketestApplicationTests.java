4445b7d1c9e90155712894ca115f446ea4a1b91e
Adrian Mille <adrmille@users.noreply.github.com>
2018-12-23 20:38:34 +0100

    #1 Init Springboot project

---
+++ smoketest/src/test/java/com/amille/smoketest/SmoketestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.amille.smoketest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmoketestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
